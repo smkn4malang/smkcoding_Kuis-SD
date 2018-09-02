802883b685820528fb922eda8380b25fc7adc4af
TIARA <tiararomanisti2002@gmail.com>
2018-09-02 11:37:26 +0700

    add screenshots

---
+++ app/src/main/java/com/dycode/edu/kuissdapp/SoaalActivity.java
@@ -0,0 +1,13 @@
+package com.dycode.edu.kuissdapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoaalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soaal);
+    }
+}
